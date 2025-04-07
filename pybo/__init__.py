ebd38728fa8d183ca46853cb01a3d5ccc657034f
백지연 <qorwldus123@gmail.com>
2025-04-07 10:36:56 +0900

    config 변경

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -5,20 +5,10 @@ from sqlalchemy import MetaData
 
 import config
 
-naming_convention = {
-    "ix": 'ix_%(column_0_label)s',
-    "uq": "uq_%(table_name)s_%(column_0_name)s",
-    "ck": "ck_%(table_name)s_%(column_0_name)s",
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s"
-}
-db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
-migrate = Migrate()
-
-
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(config)
+    app.config.from_envvar('APP_CONFIG_FILE')
+    # app.config.from_object(config)
 
     # ORM
     db.init_app(app)
@@ -40,3 +30,13 @@ def create_app():
     app.jinja_env.filters['datetime'] = format_datetime
 
     return app
+
+naming_convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
+migrate = Migrate()
