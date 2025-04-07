dfa13df5e1ab4ec4285fed40eba54439963cc283
백지연 <qorwldus123@gmail.com>
2025-04-07 15:25:20 +0900

    오류등록

--- config/production.py
+++ config/production.py
@@ -1,4 +1,6 @@
 from logging.config import dictConfig
+from config.default import *
+
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
